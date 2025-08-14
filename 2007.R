85bf41ada5cae4a0dc9e230ef029d56dcfddbaba
zyang91 <zhanchao@upenn.edu>
2025-08-14 14:46:13 -0400

    u

--- 2007.R
+++ 2007.R
@@ -1,15 +1,35 @@
 library(foreign)
 library(tidyverse)
 
-trip <- read.dbf("data/2007/tr_viajes.dbf", as.is = TRUE)
+#trip <- read.dbf("data/2007/tr_viajes.dbf", as.is = TRUE)
 
-trip_2007summary<-trip%>%
-  group_by(SORDENTRAN)%>%
+trip_2007<- read.csv("data/2007/trip_2007.csv")
+
+library(tidyr)
+
+
+
+trip_2007 <- trip_2007 %>%
+  separate(SORDENTRAN, into = paste0("trip", 1:7), sep = 1:6) %>%
+  mutate(across(starts_with("trip"), ~ na_if(., "0")))
+
+single_mode<- trip_2007 %>%
+  filter(is.na(trip2))
+
+single_mode<-single_mode%>%
+  group_by(trip1)%>%
   summarise(
     count = n()
-  )%>%
-  mutate(
-    percent = count / sum(count) * 100
   )
 
-write.csv(trip, "data/2007/trip_2007.csv", row.names = FALSE)
+
+trip3_or_more<- trip_2007 %>%
+  filter(!is.na(trip3))
+trip4_or_more<- trip_2007 %>%
+  filter(!is.na(trip4))
+trip5_or_more<- trip_2007 %>%
+  filter(!is.na(trip5))
+trip6_or_more<- trip_2007 %>%
+  filter(!is.na(trip6))
+
+
--- 2017.R
+++ 2017.R
@@ -1,4 +1,11 @@
 library(foreign)
 library(tidyverse)
 
-trip <- read.dbf("TVIAJE.DBF", as.is = TRUE)
+trip_2017<- read.csv("data/2017/trip_2017.csv")
+
+P5_15_02<-trip_2017%>%
+  group_by(P5_15_02)%>%
+  summarise(
+    count = n()
+  )
+  
